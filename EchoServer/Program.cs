edc28bcf2f5b5b641e7221775e80d6bd95a552d1
Gabriel Castellani de Oliveira <gabriel.castellani@remotatec.com.br>
2026-08-12 15:51:47 -0300

    Exemplo de arquitetura cliente-servidor iterativo

---
+++ EchoServer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var listener = new TcpListener(IPAddress.Loopback, 5000);
+listener.Start();
+
+Console.WriteLine("Servidor ITERATIVO escutando na porta 5000");
+
+while (true)
+{
+    using var client = await listener.AcceptTcpClientAsync();
+
+    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] aceito: {client.Client.RemoteEndPoint}");
+
+    var stream = client.GetStream();
+    var buffer = new byte[1024];
+    int n;
+
+    while ((n = await stream.ReadAsync(buffer)) > 0)
+        await stream.WriteAsync(buffer.AsMemory(0, n));
+
+    Console.WriteLine("Cliente finalziado");
+}
